2b6fb41f72740f9ea7087d4ad29677b8ac70e122
Charlotte Flygare <charlotte.flygare@gmail.com>
2018-12-20 08:19:46 +0100

    New gif and change of bg

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -11,92 +11,101 @@ class App extends React.Component {
   render() {
     return (
       <div>
-      <div className="wrapper">
-        <Hero />
-        <div className="wrapper-title">
-          <h2>Hur går det till?</h2>
-        </div>
+        <div className="wrapper">
+          <Hero />
+          <div className="wrapper-title">
+            <h2>Hur går det till?</h2>
+          </div>
 
-        <Grid>
-          <div className="how-container">
-            <div className="how-container-text">
-              <h4>Tempor incididunt</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-              ullamco laboris nisi ut aliquip ex commodo consequat.
-              </p>
-            </div>
-            <div className="how-container-image">
-              <div className="how-container-icons">
-                <div className="how-container-icons-text">
-                  <img src="./images/djupast_sv.png" alt="deepest" />
-                  <p>djupast</p>
-                </div>
-                <div className="how-container-icons-text">
-                  <img src="./images/fortast_sv.png" alt="quickest" />
-                  <p>fortast</p>
-                </div>
-                <div className="how-container-icons-text">
-                  <img src="./images/langst_sv.png" alt="longest" />
-                  <p>längst</p>
+          <Grid>
+            <div className="how-container">
+              <div className="how-container-text">
+                <h4>Tävlingsklasser</h4>
+                <p>
+                  Sömnen har en egen dygnsrytm men kan påverkas av våra levnadsvanor.
+                  Under nätterna har vi perioder av djupare och lättare sömn samt perioder
+                  då vi drömmer. Dessa perioder beror på hur hjärnans aktivitet varierar
+                  under våra olika sömnstadier.
+                </p>
+              </div>
+              <div className="how-container-image">
+                <div className="how-container-icons">
+                  <div className="how-container-icons-text">
+                    <img src="./images/moonstar.gif" alt="deepest" />
+                    <p>djupast</p>
+                  </div>
+                  <div className="how-container-icons-text">
+                    <img src="./images/clock.png" alt="quickest" />
+                    <p>fortast</p>
+                  </div>
+                  <div className="how-container-icons-text">
+                    <img src="./images/curve.gif" alt="longest" />
+                    <p>längst</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-
-          <div className="how-container">
-            <div className="how-container-image-gender">
-              <div className="how-container-icons-gender">
-                <div className="how-container-icons-text">
-                  <img src="./images/damherr.png" alt="woman" />
-                  <p>dam</p>
-                </div>
-                <div className="how-container-icons-text">
-                  <img src="./images/damherr.png" alt="man" />
-                  <p>herr</p>
+            <div className="how-container">
+              <div className="how-container-image-gender">
+                <div className="how-container-icons-gender">
+                  <div className="how-container-icons-text">
+                    <img src="./images/damherr.png" alt="woman" />
+                    <p>man</p>
+                  </div>
+                  <div className="how-container-icons-text">
+                    <img src="./images/damherr.png" alt="man" />
+                    <p>kvinna</p>
+                  </div>
                 </div>
               </div>
+              <div className="how-container-text-gender">
+                <h4>Kön</h4>
+                <p>
+                  Hur mycket man sover är väldigt individuellt och
+                  varierar från person till person. De flesta sover
+                  mellan 5och 10 timmar per natt. Kvinnor behöver
+                  generellt sätt lite mer sömn än män. Det förhöjda
+                  sömnbehovet hos kvinnor har speciellt två anledningar:
+                  kroppstemperaturen och hormoner.
+                </p>
+              </div>
             </div>
-            <div className="how-container-text-gender">
-              <h4>Tempor incididunt</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-              ullamco laboris nisi ut aliquip ex commodo consequat.
-              </p>
-            </div>
-
-          </div>
           </Grid>
-          </div>
+        </div>
 
         <div className="why-container-wrapper">
-            <Grid>
-          <div className="wrapper-title">
-            <h2>Lider du av sömnsvårigheter?</h2>
-          </div>
-          <div className="why-container">
-            <div className="why-container-text">
-              <p>Boka tid med någon av våra psykologer och få hjälp med att förbättra din sömn.</p>
-              <h4><span>Chatta med någon av våra psykologer</span></h4>
-              <button className="lasmer-button" type="button">Läs mer</button>
+          <Grid>
+            <div className="wrapper-title-somn">
+              <h2>Lider du av sömnsvårigheter?</h2>
             </div>
-            <div className="why-container-icons-gif">
-              <img src="./images/chaticon2.gif" alt="" />
+            <div className="why-container">
+              <div className="why-container-text">
+                <p>
+                  Sömnproblem är vanligt och beror ofta på stress eller större förändringar
+                  i livet. De allra flesta upplever att de lider av sömnbrist under kortare
+                  perioder. Om sömnbesvären inte går över av sig själva utan förvärras kan
+                  du behöva hjälp att bryta den onda cirkeln.
+                  <span>
+                  Boka tid med någon av våra psykologer och få hjälp med att förbättra din sömn.
+                  </span>
+                </p>
+                <h4>
+                  <span>
+                  Chatta med någon av våra psykologer
+                  </span>
+                </h4>
+                <button className="lasmer-button" type="button">Läs mer</button>
+              </div>
+              <div className="why-container-icons-gif">
+                <img src="./images/chaticon2.gif" alt="" />
+              </div>
             </div>
-          </div>
           </Grid>
-
         </div>
-
-
         <Footer />
-        </div>
-
+      </div>
     )
   }
-
 }
 
 export default App
--- code/src/components/footer/footer.js
+++ code/src/components/footer/footer.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Button from "../button/button.js"
 import Grid from "../grid/grid.js"
-
 import "./footer.scss"
 
 class Footer extends React.Component {
@@ -10,30 +9,46 @@ class Footer extends React.Component {
     return (
       <div className="footer-section">
         <Grid>
-        <div className="footer-text">
-          <div className="footer-image">
-            <img src="./images/logo_icon.png" alt="heart" />
-          </div>
-          <div className="footer-text-column">
-            <h4>Företaget</h4>
-            <p><span>Om Min Doktor </span></p>
-            <p><span>Jobba hos oss </span></p>
-            <p><span>Utmärkelser</span></p>
-            <p><span>Press</span></p>
-          </div>
-          <div className="footer-text-column">
-            <h4>Tjänsten</h4>
-            <p><span>Cookies </span></p>
-            <p><span>Policy </span></p>
-            <p><span>Villkor</span></p>
-            <p><span>Responsible Disclosure Policy</span></p>
-            <p><span>Användarregler</span></p>
-
-          </div>
-
+          <div className="footer-text">
+            <div className="footer-image">
+              <img src="./images/logo_icon.png" alt="heart" />
+            </div>
+            <div className="footer-text-column">
+              <h4>Företaget</h4>
+              <p>
+                <span>
+                Om Min Doktor
+                </span>
+              </p>
+              <p>
+                <span>Jobba hos oss</span>
+              </p>
+              <p>
+                <span>Utmärkelser</span>
+              </p>
+              <p><span>Press</span></p>
+            </div>
+            <div className="footer-text-column">
+              <h4>Tjänsten</h4>
+              <p>
+                <span>Cookies</span>
+              </p>
+              <p>
+                <span>Policy </span>
+              </p>
+              <p>
+                <span>Villkor</span>
+              </p>
+              <p>
+                <span>Responsible Disclosure Policy</span>
+              </p>
+              <p>
+                <span>Användarregler</span>
+              </p>
+            </div>
         </div>
         </Grid>
-      </div>
+        </div>
     )
   }
 
